52d9049a9b8ff955e0014bf0db7f22242de94c60
Matt Works <mworks4905@gmail.com>
2016-09-22 16:12:44 -0600

    Complete migrate and seed of part 4

---
+++ seeds/3_favorites.js
@@ -0,0 +1,14 @@
+'use strict';
+exports.seed = function(knex) {
+  
+  return knex('favorites').del()
+    .then(() => {
+      return knex('favorites').insert([{
+        id: 1,
+        book_id: 1,
+        user_id: 1,
+        created_at: new Date('2016-06-29 14:26:16 UTC'),
+        updated_at: new Date('2016-06-29 14:26:16 UTC')
+      }]);
+    });
+};
