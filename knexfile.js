b245a001b32bdda8eb37df9339e7eedbcc6b5f5b
Matt Works <mworks4905@gmail.com>
2016-09-23 16:26:28 -0600

    Finish authentication

--- knexfile.js
+++ knexfile.js
@@ -9,6 +9,11 @@ module.exports = {
   test: {
     client: 'pg',
     connection: 'postgres://localhost/bookshelf_dev'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 
 };
--- routes/session.js
+++ routes/session.js
@@ -1,10 +1,48 @@
 'use strict';
 
 const express = require('express');
-
-// eslint-disable-next-line new-cap
 const router = express.Router();
+const knex = require('../knex');
+const bcrypt = require('bcrypt');
+const humps = require('humps');
+const boom = require('boom');
 
-// YOUR CODE HERE
+router.get('/session', (req, res, next) => {
+  if(req.session.userInfo){
+    res.send(true)
+  }
+  else{
+    res.send(false)
+  }
+})
+
+router.post('/session', (req, res, next) => {
+  knex('users').where('email', req.body.email)
+  .then((results) => {
+    if(results.length == 0){
+      throw boom.create(400, 'Bad email or password')
+    }
+    else{
+      var user = results[0]
+      var pwMatch = bcrypt.compareSync(req.body.password, user.hashed_password)
+      if(pwMatch === false){
+        throw boom.create(400, 'Bad email or password')
+      }
+      else{
+        delete user.hashed_password
+        req.session.userInfo = user
+        res.send(humps.camelizeKeys(user))
+      }
+    }
+  })
+  .catch((err) => {
+    next(err)
+  })
+})
+
+router.delete('/session', (req, res, next) => {
+  req.session = null
+  res.send(true)
+})
 
 module.exports = router;
