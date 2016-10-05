9fce8e01e618a8077274c8a00a199adc2eea2519
Matt Works <mworks4905@gmail.com>
2016-10-05 14:38:42 -0600

    Fix duplicate email error msg

--- routes/users.js
+++ routes/users.js
@@ -6,27 +6,38 @@ const bcrypt = require('bcrypt-as-promised')
 const knex = require('../knex')
 const humps = require('humps')
 const session = require('cookie-session')
+const boom = require('boom')
 
 router.post('/users', (req, res, next) => {
-    bcrypt.hash(req.body.password, 12)
-        .then((hashed_password) => {
-            return knex('users')
-                .insert({
-                    first_name: req.body.firstName,
-                    last_name: req.body.lastName,
-                    email: req.body.email,
-                    hashed_password: hashed_password
-                }, '*')
-        })
-        .then((users) => {
-            const user = users[0]
-            delete user.hashed_password
-            req.session.userInfo = user;
-            res.send(humps.camelizeKeys(user))
-        })
-        .catch((err) => {
-            next(err)
-        })
+  knex('users')
+  .where('email', req.body.email)
+  .first()
+  .then((exists) => {
+    if(exists){
+      throw boom.create(400, 'This email is already in use')
+    }
+    return bcrypt.hash(req.body.password, 12)
+  })
+
+    .then((hashed_password) => {
+      return knex('users')
+      .insert({
+        first_name: req.body.firstName,
+        last_name: req.body.lastName,
+        email: req.body.email,
+        hashed_password: hashed_password,
+
+      }, '*')
+    })
+    .then((users) => {
+      const user = users[0]
+      delete user.hashed_password
+      req.session.userInfo = user;
+      res.send(humps.camelizeKeys(user))
+    })
+    .catch((err) => {
+      next(err)
+    })
 });
 
 module.exports = router;
